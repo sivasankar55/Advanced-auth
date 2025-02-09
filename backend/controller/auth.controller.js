d9bc78b4f219c4bde5ea7ea2659b20126d520cd8
sivasankar55 <sivasankar8875@gmil.com>
2025-02-09 22:14:33 +0530

    auth controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,16 +1,46 @@
 import {User} from "../model/user.model.js";
-
+import bcryptjs from "bcryptjs";
+ import {generateTokenAndSetCookie} from "../utils/generateTokenAndSetCookie.js";
 export const signup= async (req,res) => {
   const {email, password, name} = req.body;
 
   try {
-    if(email || password || neme){
+    if(!email || !password || !name){
         throw new Error("All fields are required");
     }
-    const userAlreadyExits = await User.findOne({email})
-    if(userAlreadyExits){
+    const userAlreadyExists = await User.findOne({email})
+    console.log("userAlreadyExists", userAlreadyExists);
+
+    if(userAlreadyExists){
         return res.status(400).json({success:false,message:"User already exits"});
     }
+
+    const hashedPassword = await bcryptjs.hash(password,10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    })
+
+    await user.save();
+
+     //jwt
+     generateTokenAndSetCookie(res, user._id);
+     
+    res.status(201).json({
+      success:true,
+      message:"User created successfully",
+      user:{
+        ...user._doc,
+        password:undefined
+      },
+    });
+
+   
   } catch (error) {
     res.status(400).json({success:false, message:error.message});
   }
