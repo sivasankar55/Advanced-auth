2594b5891ee6c89394bff6af2f4575ce09af1ef3
sivasankar55 <sivasankar8875@gmil.com>
2025-02-11 21:46:04 +0530

    setting up the mailtrap and and 6 digit code

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import {User} from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
  import {generateTokenAndSetCookie} from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail } from "../mailtrap/emails.js";
+ 
 export const signup= async (req,res) => {
   const {email, password, name} = req.body;
 
@@ -30,6 +32,8 @@ export const signup= async (req,res) => {
 
      //jwt
      generateTokenAndSetCookie(res, user._id);
+
+      await sendVerificationEmail(user.email,verificationToken);
      
     res.status(201).json({
       success:true,
