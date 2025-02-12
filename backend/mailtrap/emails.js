012925132c2af51527fb7c0d31344ff74cad34bd
sivasankar55 <sivasankar8875@gmil.com>
2025-02-12 22:00:15 +0530

    verify throgh code

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -11,12 +11,35 @@ try{
         to:recipient,
         subject:"Verify your email",
         html:VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}",verificationToken),
-        category:"Email Verification"
-    })
+        category:"Email Verification",
+    });
 
-    console.log("Email sent successfully",response)
+    console.log("Email sent successfully",response);
+
 }catch (error){
     console.log("Error sending verification",error);
     throw new Error(`Error sending verification email: ${error}`)
 }
 }
+
+export const sendWelcomeEmail = async (email,name) => {
+ const recipient = [{email}];
+
+ try {
+    
+    const response = await mailtrapClient.send({
+        from:sender,
+        to:recipient,
+        template_uuid : "46334c6e-7031-427a-841b-abfb73214426",
+        template_variables: {
+            company_info_name: "Auth Company",
+            name: name,
+          },
+    });
+
+    console.log("Welcome email sent successfully", response);
+ } catch (error) {
+    console.error("Error sending welcome email",error);
+    throw new Error(`Error sending welcome email:${error}`);
+ }
+};
--- backend/router/auth.router.js
+++ backend/router/auth.router.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, logout, signup } from "../controller/auth.controller.js";
+import { login, logout, signup, verifyEmail } from "../controller/auth.controller.js";
 
 const router = express.Router()
 
@@ -8,5 +8,7 @@ router.post("/signup",signup )
 router.post("/login",login);
 
 router.post("/logout",logout);
+
+router.post("/verify-email",verifyEmail);
 
 export default router;
