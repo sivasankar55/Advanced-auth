250f4ceeb989cf76b15e494dc79aa31c0c571e36
sivasankar55 <sivasankar8875@gmil.com>
2025-02-13 20:27:44 +0530

    forgotPassword functionality

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,4 +1,4 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
+import { PASSWORD_RESET_REQUEST_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
 import { mailtrapClient,sender } from "./mailtrap.config.js"
 
 export const sendVerificationEmail = async(email,verificationToken) => {
@@ -43,3 +43,22 @@ export const sendWelcomeEmail = async (email,name) => {
     throw new Error(`Error sending welcome email:${error}`);
  }
 };
+
+export const sendPasswordResetEmail = async (email,resetURL) => {
+    const recipient = [{email}];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Reset your password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+            category:"Password Reset",
+        })
+
+    } catch (error) {
+        console.error(`Error sending password reset email`, error);
+
+        throw new Error(`Error sending password reset email:${error}`);
+    }
+}
--- backend/router/auth.router.js
+++ backend/router/auth.router.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, logout, signup, verifyEmail } from "../controller/auth.controller.js";
+import { login, logout, signup, verifyEmail ,forgotPassword, resetPassword } from "../controller/auth.controller.js";
 
 const router = express.Router()
 
@@ -11,4 +11,7 @@ router.post("/logout",logout);
 
 router.post("/verify-email",verifyEmail);
 
+router.post("/forgot-password", forgotPassword);
+
+router.post ("/reset-password/:token", resetPassword)
 export default router;
