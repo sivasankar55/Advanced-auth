ee8d80f2257d167d51c52ee1fe5dc3b771969a4e
sivasankar55 <sivasankar8875@gmil.com>
2025-02-15 12:58:44 +0530

    signuppage and loginpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-
+import {Route, Routes} from "react-router-dom";
+import FloatingShape from "./components/FloatingShape";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
-  
-
   return (
     <>
-  <h1 className='text-red-500 text-5xl'>hello world</h1>
+  <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+
+    <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0}/>
+    <FloatingShape color="bg-emerald-500" size="w-64 h-48" top="70%" left="80%" delay={5}/>
+    <FloatingShape color="bg-green-500" size="w-32 h-32" top="40%" left="-10%" delay={2}/>
+
+    <Routes>
+      <Route path="/" element={"Home"} />
+      <Route path="/signup" element={<SignUpPage/>}/>
+      <Route path="/login" element= {<LoginPage/>}/>
+    </Routes>
+  </div>
     </>
   )
 }
